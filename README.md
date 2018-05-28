b97fddfe7a43bafc866622cea057735f15b5d662
Yuanjianxin <1814888800@qq.com>
2018-05-28 16:11:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# yue-star-detector-handler
---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yuanjianxin on 2018/5/28.
+ */
+const StarDetectorHandler=require('./lib/StarDetectorHandler');
+module.exports=StarDetectorHandler;
